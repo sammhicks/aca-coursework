4b36a2288fda5799617f8d0b4dfc81dd0d690961
Samuel Hicks <sh14820@my.bristol.ac.uk>
2017-10-18 16:27:44 +0100

    Fixed optimised to use new pneumonics

--- compiler/optimiser/optimiser.pl
+++ compiler/optimiser/optimiser.pl
@@ -12,8 +12,16 @@ optimise(A, A).
 
 
 optimise_once(Old, New) :-
-	nth0(PC, Old, j(Offset, InvFlag, CondFlag, Register), Rest),
-	Target is PC + Offset,
-	nth0(Target, Old, j(Target_Offset, false, "1", -1)),
-	New_Offset is Offset + Target_Offset,
-	nth0(PC, New, j(New_Offset, InvFlag, CondFlag, Register), Rest).
+	nth0(PC, Old, j(Offset), Rest),
+	Target is PC + 1 + Offset,
+	nth0(Target, Old, j(Target_Offset)),
+	New_Offset is Offset + Target_Offset + 1,
+	nth0(PC, New, j(New_Offset), Rest).
+
+optimise_once(Old, New) :-
+	nth0(PC, Old, cj(Offset, Invert, Cond, R1), Rest),
+	Target is PC + 1 + Offset,
+	nth0(Target, Old, j(Target_Offset)),
+	New_Offset is Offset + Target_Offset + 1,
+	nth0(PC, New, cj(New_Offset, Invert, Cond, R1), Rest).
+
